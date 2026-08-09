336154565d1b3bfc96610c446800e4ac4a3d44dc
Aidan Feldman <aidan.feldman@gmail.com>
2026-08-09 16:05:35 +0000

    format SQL through SQL Tools

--- setup.sql
+++ setup.sql
@@ -5,7 +5,8 @@ INSTALL httpfs;
 LOAD httpfs;
 
 -- https://duckdb.org/community_extensions/extensions/cache_httpfs
-INSTALL cache_httpfs FROM community;
+INSTALL cache_httpfs
+FROM community;
 LOAD cache_httpfs;
 
 -- https://duckdb.org/docs/current/core_extensions/spatial/overview#installing-and-loading
@@ -18,11 +19,10 @@ SET geometry_always_xy = true;
 -- https://docs.source.coop/data-proxy
 -- https://github.com/source-cooperative/docs.source.coop/pull/29
 CREATE OR REPLACE SECRET source_coop (
-    TYPE s3,
-    PROVIDER config,
-    ENDPOINT 'data.source.coop',
-    URL_STYLE 'path'
-    -- If you're working with multiple S3-compatible sources (beyond the Data Proxy), you'll want to only apply this SECRET to the Source Cooperative account name(s).
-    -- https://duckdb.org/docs/current/configuration/secrets_manager#creating-multiple-secrets-for-the-same-service-type
-    -- SCOPE 's3://kerner-lab'
-);
+        TYPE s3,
+        PROVIDER config,
+        ENDPOINT 'data.source.coop',
+        URL_STYLE 'path' -- If you're working with multiple S3-compatible sources (beyond the Data Proxy), you'll want to only apply this SECRET to the Source Cooperative account name(s).
+        -- https://duckdb.org/docs/current/configuration/secrets_manager#creating-multiple-secrets-for-the-same-service-type
+        -- SCOPE 's3://kerner-lab'
+    );
