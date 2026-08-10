eca6da6aab9be97fdea738b9d1dcd1aa76fd651f
Aidan Feldman <aidan.feldman@gmail.com>
2026-08-09 18:20:15 -0600

    refactor color mapping

--- helpers.py
+++ helpers.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import matplotlib
+import pandas as pd
 from duckdb import DuckDBPyConnection
+from lonboard.colormap import apply_continuous_cmap
 
 
 def run_script_in_db(conn: DuckDBPyConnection, path: Path | str):
@@ -8,3 +11,14 @@ def run_script_in_db(conn: DuckDBPyConnection, path: Path | str):
         sql = f.read()
 
     conn.execute(sql)
+
+
+def to_continuous_color_map(values: pd.Series, cmap: str):
+    """cmap options: https://matplotlib.org/stable/gallery/color/colormap_reference.html"""
+
+    vmin = values.min()
+    vmax = values.max()
+    scaled = (values - vmin) / (vmax - vmin)
+
+    cmap_obj = matplotlib.colormaps[cmap]
+    return apply_continuous_cmap(scaled, cmap_obj)
