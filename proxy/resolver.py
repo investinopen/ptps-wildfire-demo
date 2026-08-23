917237759d8b74bde6b6eb008eadcc165fced48a
Aidan Feldman <aidan.feldman@gmail.com>
2026-08-23 02:20:29 -0400

    refactor the Resolver

--- proxy/resolver.py
+++ proxy/resolver.py
@@ -2,20 +2,20 @@ import pandas as pd
 
 
 class Resolver:
-    rescued_data: pd.DataFrame
+    drp_rescues: pd.DataFrame
 
     def __init__(self) -> None:
         self.refresh()
 
     def refresh(self):
         # This is the data behind https://portal.datarescueproject.org/datasets/. We'll use https://github.com/datarescueproject/portal/pull/26 if/when it's merged.
-        self.rescued_data = pd.read_csv(
+        self.drp_rescues = pd.read_csv(
             "https://raw.githubusercontent.com/datarescueproject/portal/refs/heads/main/baserow_exports/datarescue_backups.csv",
             dtype_backend="pyarrow",
         )
 
-    def _get_match(self, boolean_index: pd.Series[bool]) -> str | None:
-        matches = self.rescued_data[boolean_index]
+    def _get_drp_match(self, boolean_index: pd.Series[bool]) -> str | None:
+        matches = self.drp_rescues[boolean_index]
         if len(matches) > 0:
             row = matches.iloc[0]
             if pd.notna(row["download_location"]):
@@ -23,10 +23,17 @@ class Resolver:
 
         return None
 
-    def find_fallback_url(self, url: str) -> str | None:
-        is_exact_match = self.rescued_data["url"] == url
-        # Look for record where the provided URL is based on the record's URL. This is intended to catch request URLs that are a sub-path, have parameters, etc. The matching could be even more robust.
-        is_partial_match = self.rescued_data["url"].apply(
+    def _get_drp_exact_match(self, url: str) -> str | None:
+        is_exact_match = self.drp_rescues["url"] == url
+        return self._get_drp_match(is_exact_match)
+
+    def _get_drp_partial_match(self, url: str) -> str | None:
+        """Look for record where the provided URL is based on the record's URL. This is intended to catch request URLs that are a sub-path, have parameters, etc. The matching could be even more robust."""
+
+        is_partial_match = self.drp_rescues["url"].apply(
             lambda original_url: pd.notna(original_url) and url.startswith(original_url)
         )
-        return self._get_match(is_exact_match) or self._get_match(is_partial_match)
+        return self._get_drp_match(is_partial_match)
+
+    def find_fallback_url(self, url: str) -> str | None:
+        return self._get_drp_exact_match(url) or self._get_drp_partial_match(url)
