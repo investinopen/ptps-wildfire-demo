a9e885285dca111d2cb6eae50130cbe73728de0a
Aidan Feldman <aidan.feldman@gmail.com>
2026-08-24 15:57:30 -0400

    handle timeout from Internet Archive

--- ptps_wildfire_demo/proxy/internet_archive_client.py
+++ ptps_wildfire_demo/proxy/internet_archive_client.py
@@ -20,7 +20,7 @@ class InternetArchiveClient:
         self.secret_key = os.environ.get("INTERNET_ARCHIVE_SECRET_KEY")
 
     async def request(
-        self, url: str, *, method="GET", timeout=5, params: dict | None = None
+        self, url: str, *, method="GET", timeout=10, params: dict | None = None
     ):
         # not using the official package because we want async support
         # https://archive.org/developers/internetarchive/index.html
@@ -43,9 +43,12 @@ class InternetArchiveClient:
     async def get_match(self, url: str) -> str | None:
         """https://archive.org/help/wayback_api.php"""
 
-        response = await self.request(
-            "https://archive.org/wayback/available", params={"url": url}
-        )
+        try:
+            response = await self.request(
+                "https://archive.org/wayback/available", params={"url": url}
+            )
+        except httpx.TimeoutException:
+            return None
 
         try:
             results = response.json()["archived_snapshots"]
@@ -57,4 +60,5 @@ class InternetArchiveClient:
     async def save(self, url: str):
         """https://help.archive.org/help/save-pages-in-the-wayback-machine/"""
 
+        # this endpoint waits for the page to be archived, so use a longer timeout
         return await self.request(f"https://web.archive.org/save/{url}", timeout=20)
--- tests/test_internet_archive_client.py
+++ tests/test_internet_archive_client.py
@@ -1,4 +1,6 @@
+import httpx
 import pytest
+from pytest_httpx import HTTPXMock
 
 from ptps_wildfire_demo.proxy.internet_archive_client import InternetArchiveClient
 
@@ -11,3 +13,10 @@ async def client(httpx_client):
 async def test_save(client):
     response = await client.save("https://investinopen.org/")
     assert 200 <= response.status_code < 400
+
+
+async def test_get_match_timeout(client, httpx_mock: HTTPXMock):
+    httpx_mock.add_exception(httpx.TimeoutException("Timed out"))
+
+    match = await client.get_match("https://investinopen.org/")
+    assert match is None
